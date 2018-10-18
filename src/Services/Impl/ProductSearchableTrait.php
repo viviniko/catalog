df1265a7efa7e0860769d6f1bc787e3093b219b8
viviniko <viviniko.shop@gmail.com>
2018-10-18 20:13:18 +0800

    :trollface:

--- src/Services/Impl/ProductSearchableTrait.php
+++ src/Services/Impl/ProductSearchableTrait.php
@@ -69,7 +69,7 @@ trait ProductSearchableTrait
         $searchArray['recommend_score'] = $searchArray['hot_score'] * 3 + $searchArray['new_score'] * 2 + $searchArray['promote_score'] * 2;
         $searchArray['favorite_count'] = $favoriteService ? $favoriteService->count($product) : 0;
 
-        $searchArray['amount'] = (float)$searchArray['amount'];
+        $searchArray['amount'] = (float)$searchArray['amount']->value;
         $searchArray['weight'] = (float)$searchArray['weight'];
         $searchArray['sort'] = (int)$searchArray['sort'];
 
