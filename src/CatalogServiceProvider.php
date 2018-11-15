b7773b0f0f2c95b63f4bfda9e95c0c66d90f9771
viviniko <viviniko.shop@gmail.com>
2018-11-15 18:26:46 +0800

    :trollface:

--- src/CatalogServiceProvider.php
+++ src/CatalogServiceProvider.php
@@ -4,8 +4,6 @@ namespace Viviniko\Catalog;
 
 use Illuminate\Support\Facades\Event;
 use Viviniko\Catalog\Console\Commands\CatalogTableCommand;
-use Viviniko\Catalog\Listeners\CategoryEventSubscriber;
-use Viviniko\Catalog\Listeners\ItemEventSubscriber;
 use Viviniko\Catalog\Models\Category;
 use Viviniko\Catalog\Models\Product;
 use Viviniko\Catalog\Models\Item;
@@ -45,9 +43,6 @@ class CatalogServiceProvider extends BaseServiceProvider
         Category::observe(CategoryObserver::class);
         Item::observe(ItemObserver::class);
         Product::observe(ProductObserver::class);
-
-        Event::subscribe(CategoryEventSubscriber::class);
-        Event::subscribe(ItemEventSubscriber::class);
 
         $config = $this->app['config'];
 
